89fce7ebde5fab02028064b22f5f1add8d2a7dc5
Juan Carlos <jcmrojas29@gmail.com>
2023-05-13 18:34:37 -0500

    comentando conexion de prueba

--- src/com/feria/principal/Main.java
+++ src/com/feria/principal/Main.java
@@ -3,12 +3,12 @@ package com.feria.principal;
 import com.feria.controlador.WindowControlador;
 import com.feria.vista.Window;
 
-import com.feria.dao.DbConnection;
+//import com.feria.dao.DbConnection;
 
 public class Main {
     
     public static void main(String[] args) {
-        new DbConnection();
+//        new DbConnection();
         
         WindowControlador controlador = new WindowControlador(new Window());
         controlador.mostrarVentana();
