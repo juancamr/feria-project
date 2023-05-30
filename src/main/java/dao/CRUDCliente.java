c882e38ae127e462cebffb55f03ea18baf4a421e
Piero Jurupe <pierojurupeq@gmail.com>
2023-05-30 01:14:53 -0500

    sntahoetuhstahoeu

---
+++ src/main/java/dao/CRUDCliente.java
@@ -0,0 +1,18 @@
+
+package main.java.dao;
+import main.java.dao.DbConnection;
+/**
+ *
+ * @author piero
+ */
+public class CRUDCliente extends DbConnection {
+    public static void crearCliente() {
+        try {
+            
+        }
+    }
+    
+    public static Cliente getCliente() {
+        ret
+    }
+}
