29ef9a2f404da275954f1fd4174ee97c6d828da5
Juan Carlos <jcmrojas29@gmail.com>
2023-06-29 18:09:33 -0500

    crear modelo Chart con inicializacion de crud

--- src/dao/CRUDChart.java
+++ src/dao/CRUDChart.java
@@ -1,2 +1,59 @@
-package dao;public class CRUDChart {
+package dao;
+
+import interfaces.Querys;
+import modelo.Chart;
+import modelo.Response;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CRUDChart extends BaseCRUD<Chart> implements Querys {
+
+    public static CRUDChart crudChart;
+    @Override
+    public Response<Chart> add(Chart data) {
+        return null;
+    }
+
+    @Override
+    public Response<Chart> get(int id) {
+        return null;
+    }
+
+    @Override
+    public Response<Chart> getMany(int id) {
+        return null;
+    }
+
+    @Override
+    public Response<Chart> getAll() {
+        return null;
+    }
+
+    @Override
+    public Response<Chart> edit(int id) {
+        return null;
+    }
+
+    @Override
+    public Response<Chart> delete(int id) {
+        return null;
+    }
+
+    @Override
+    public void makeRequest(Chart data, String sql) throws SQLException {
+
+    }
+
+    @Override
+    public Chart makeResponse(ResultSet rs) throws SQLException {
+        return null;
+    }
+
+    public static CRUDChart getInstance() {
+        if (crudChart == null) {
+            crudChart = new CRUDChart();
+        }
+        return crudChart;
+    }
 }
--- src/modelo/Chart.java
+++ src/modelo/Chart.java
@@ -9,8 +9,65 @@ public class Chart {
     private String yName;
     private List xData;
     private List yData;
-    
-    public Chart() {}
+
+    public Chart() {
+    }
+
+    private Chart(Builder builder) {
+        this.id = builder.id;
+        this.title = builder.title;
+        this.xName = builder.xName;
+        this.yName = builder.yName;
+        this.xData = builder.xData;
+        this.yData = builder.yData;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private int id;
+        private String title;
+        private String xName;
+        private String yName;
+        private List<Object> xData;
+        private List<Object> yData;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Builder setXName(String xName) {
+            this.xName = xName;
+            return this;
+        }
+
+        public Builder setYName(String yName) {
+            this.yName = yName;
+            return this;
+        }
+
+        public Builder setXData(List<Object> xData) {
+            this.xData = xData;
+            return this;
+        }
+
+        public Builder setYData(List<Object> yData) {
+            this.yData = yData;
+            return this;
+        }
+
+        public Chart build() {
+            return new Chart(this);
+        }
+    }
 
     public int getId() {
         return id;
